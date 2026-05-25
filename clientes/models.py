ba0d43fa1c941c7cdc283e236a828e1db509797b
Lindenberg <begapb@gmail.com>
2026-05-25 11:37:41 -0300

    Foi adicionado alguns campos

--- clientes/models.py
+++ clientes/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 
 class Cliente(models.Model):
-    nome = models.CharField(max_length=100)
-    cpf = models.CharField(max_length=14)
-    email = models.EmailField()
-    telefone = models.CharField(max_length=15)
+    nome_completo = models.CharField(max_length=200, verbose_name="Nome Completo")
+    CPF = models.CharField(unique=True, max_length=14, verbose_name="CPF")
+    nascimento = models.DateField(verbose_name="Data de Nascimento")
+    email = models.EmailField(unique=True, max_length=100, verbose_name="E-mail")
+    endereco = models.CharField(max_length=100, verbose_name="Endereço Completo")
+    telefone = models.CharField(max_length=20, verbose_name="Telefone")
+    pagamento = models.DateField(verbose_name="Data de Pagamento")
     
     def __str__(self):
-        return self.nome
+        return self.nome_completo
     
     plano = models.ForeignKey('planos.Plano', on_delete=models.CASCADE)
